501040570dd61e7661145e7307e82ee059cc3dfd
pripc <117996744+pripc@users.noreply.github.com>
2023-01-27 09:42:17 -0300

    Melhorando o código do modal e acrescentando o bloqueio da barra de rolagem

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let card = document.querySelector('#cardAtendimento')
+let backgroundCard = document.querySelector('#backgroundcardAtendimento')
+
+function HighlightButton(selector){
+    let button = Array.from(document.querySelectorAll(selector))
+    button[1].style.visibility="visible"
+}
+function hiddeButton(selector){
+    let button = document.querySelectorAll(selector)
+    button[1].style.visibility="hidden"  
+}
+
+document.querySelector('#atendButton').addEventListener('click', () => {
+    backgroundCard.style.display="block"
+    card.style.display="block"
+    document.querySelector('body').style.overflow="hidden"
+})
+document.getElementById('xis').addEventListener("click", () => {
+    backgroundCard.style.display="none"
+    card.style.display="none"
+    
+})
